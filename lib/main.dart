ce51cf8836797fa57857c2206e648a6525ce5374
Anthony Cantuta <anthony@cantuta.org>
2021-04-16 00:10:37 -0500

    Capítulo "Juntando todo" finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,113 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(MaterialApp(
-    home: Scaffold(body: Counter()),
-  ));
+class Product {
+  const Product({this.name});
+  final String name;
 }
 
-class Counter extends StatefulWidget {
+typedef void CartChangedCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
+      : product = product,
+        super(key: ObjectKey(product));
+
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  Color _getColor(BuildContext context) {
+    // El tema depende del BuildContext porque diferentes partes del árbol
+    // pueden tener diferentes temas.  El BuildContext indica dónde se está llevando a cabo la
+    // llamada al método build y por lo tanto qué tema usar.
+
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
   @override
-  _CounterState createState() => _CounterState();
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, !inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(product.name, style: _getTextStyle(context)),
+    );
+  }
 }
 
-class _CounterState extends State<Counter> {
-  int _counter = 0;
+class ShoppingList extends StatefulWidget {
+  ShoppingList({Key key, this.products}) : super(key: key);
 
-  void _increment() {
+  final List<Product> products;
+
+  // El framework llama createState la primera vez que un widget aparece en una ubicación
+  // dada del árbol. Si el padre vuelve a ejecutar build y usa el mismo tipo de
+  // widget (con la misma llave),
+  // el framework reutiliza el objeto State en lugar de crear un nuevo objeto State.
+
+  @override
+  _ShoppingListState createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = Set<Product>();
+
+  void _handleCartChanged(Product product, bool inCart) {
     setState(() {
-      ++_counter;
+      // Cuando un usuario cambia lo que hay en el carrito, necesitas cambiar
+      //_shoppingCart dentro de una llamada setState para activar un rebuild.
+      // El framework entonces llama a build, abajo,
+      // que actualiza la apariencia visual de la aplicación.
+
+      if (inCart)
+        _shoppingCart.add(product);
+      else
+        _shoppingCart.remove(product);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      CounterIncrementor(onPressed: _increment,),
-      CounterDisplay(count: _counter,),
-    ],);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping List'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
+    );
   }
 }
 
-class CounterDisplay extends StatelessWidget {
-  CounterDisplay({this.count});
-
-  final int count;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Count: $count');
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-
-  CounterIncrementor({this.onPressed});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(onPressed: onPressed, child: Text("Increment"),);
-  }
-
-
+void main() {
+  runApp(MaterialApp(
+    title: 'Shopping App',
+    home: ShoppingList(
+      products: <Product>[
+        Product(name: 'Eggs'),
+        Product(name: 'Flour'),
+        Product(name: 'Chocolate chips'),
+      ],
+    ),
+  ));
 }
