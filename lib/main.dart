17d7841ca6c84003cd77ba3314c40b06ca1c7f34
Anthony Cantuta <anthony@cantuta.org>
2021-04-16 23:58:13 -0500

    Hello World App Material

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,25 @@
 import 'package:flutter/material.dart';
 
-class Product {
-  const Product({this.name});
-  final String name;
-}
-
-typedef void CartChangedCallback(Product product, bool inCart);
-
-class ShoppingListItem extends StatelessWidget {
-  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
-      : product = product,
-        super(key: ObjectKey(product));
-
-  final Product product;
-  final bool inCart;
-  final CartChangedCallback onCartChanged;
-
-  Color _getColor(BuildContext context) {
-    // El tema depende del BuildContext porque diferentes partes del árbol
-    // pueden tener diferentes temas.  El BuildContext indica dónde se está llevando a cabo la
-    // llamada al método build y por lo tanto qué tema usar.
-
-    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
-  }
-
-  TextStyle _getTextStyle(BuildContext context) {
-    if (!inCart) return null;
-
-    return TextStyle(
-      color: Colors.black54,
-      decoration: TextDecoration.lineThrough,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        onCartChanged(product, !inCart);
-      },
-      leading: CircleAvatar(
-        backgroundColor: _getColor(context),
-        child: Text(product.name[0]),
-      ),
-      title: Text(product.name, style: _getTextStyle(context)),
-    );
-  }
-}
-
-class ShoppingList extends StatefulWidget {
-  ShoppingList({Key key, this.products}) : super(key: key);
-
-  final List<Product> products;
-
-  // El framework llama createState la primera vez que un widget aparece en una ubicación
-  // dada del árbol. Si el padre vuelve a ejecutar build y usa el mismo tipo de
-  // widget (con la misma llave),
-  // el framework reutiliza el objeto State en lugar de crear un nuevo objeto State.
-
-  @override
-  _ShoppingListState createState() => _ShoppingListState();
-}
-
-class _ShoppingListState extends State<ShoppingList> {
-  Set<Product> _shoppingCart = Set<Product>();
-
-  void _handleCartChanged(Product product, bool inCart) {
-    setState(() {
-      // Cuando un usuario cambia lo que hay en el carrito, necesitas cambiar
-      //_shoppingCart dentro de una llamada setState para activar un rebuild.
-      // El framework entonces llama a build, abajo,
-      // que actualiza la apariencia visual de la aplicación.
-
-      if (inCart)
-        _shoppingCart.add(product);
-      else
-        _shoppingCart.remove(product);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shopping List'),
-      ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(vertical: 8.0),
-        children: widget.products.map((Product product) {
-          return ShoppingListItem(
-            product: product,
-            inCart: _shoppingCart.contains(product),
-            onCartChanged: _handleCartChanged,
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
-
 void main() {
-  runApp(MaterialApp(
-    title: 'Shopping App',
-    home: ShoppingList(
-      products: <Product>[
-        Product(name: 'Eggs'),
-        Product(name: 'Flour'),
-        Product(name: 'Chocolate chips'),
-      ],
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter layout demo",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("The title"),
+        ),
+        body: Center(child: Text("Hello World"),),
+      )
+    );
+  }
 }
