2043af2376447e352c0df3b7f66b542cccbbb0ab
Anthony Cantuta <anthony@cantuta.org>
2021-04-17 16:36:20 -0500

    Maquetación de una card con imagenes. Practicando layouts.

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,107 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.star),
-              Icon(Icons.star),
-              Icon(Icons.star),
-            ],
-          ),
+        body: buildBody(),
+      ),
+    );
+  }
+
+  Widget buildBody() {
+    var title = Container(
+      child: Center(child: Text(
+        "Strawberry",
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
         ),
+      ),)
+    );
+
+    var subTitle = Text(
+        "Pavlova is a meringue-based dessert named after Russian ballerine Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.");
+    var star = Icon(
+      Icons.star,
+      color: Colors.green[500],
+    );
+    var stars = Row(
+      children: [
+        star,
+        star,
+        star,
+        star,
+        star,
+      ],
+    );
+
+    var rating = Text(
+      "170 Reviewss",
+      style: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w800,
+        fontFamily: 'Roboto',
+        letterSpacing: 0.5,
+      ),
+    );
+
+    final descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 10,
+      height: 2,
+    );
+
+    var iconList = DefaultTextStyle.merge(
+        style: descTextStyle,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: [Icon(Icons.kitchen), Text("PREP:"), Text("25 min.")],
+            ),
+            Column(
+              children: [Icon(Icons.restaurant), Text("FEEDS:"), Text("4-6.")],
+            ),
+            Column(
+              children: [Icon(Icons.timer), Text("COOK:"), Text("1 hr.")],
+            )
+          ],
+        ));
+
+    var leftColumn = Expanded(
+      child: Container(
+        padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            title,
+            subTitle,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                stars,
+                Expanded(
+                  child: rating,
+                ),
+              ],
+            ),
+            iconList,
+          ],
+        ),
+      ),
+    );
+
+    var rightColumn =
+    Image.asset("lib/assets/1.png", fit:  BoxFit.cover, width: 200, height: double.infinity,)
+    ;
+
+    return Scaffold(
+      body: Card(
+        child: Row(children: [
+          leftColumn,
+          rightColumn,
+        ]),
       ),
     );
   }
