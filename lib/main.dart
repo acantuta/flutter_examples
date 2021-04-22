0519b231bb10fbb733d9d9261e34661d43adb808
Anthony Cantuta <anthony@cantuta.org>
2021-04-21 21:17:27 -0500

    Construir ejemplo Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,43 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: buildBody(),
-      ),
-    );
-  }
-
-  Widget buildBody() {
-    var title = Container(
-      child: Center(child: Text(
-        "Strawberry",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w500,
-        ),
-      ),)
-    );
-
-    var subTitle = Text(
-        "Pavlova is a meringue-based dessert named after Russian ballerine Anna Pavlova. Pavlova features a crisp crust and soft, light inside, topped with fruit and whipped cream.");
-    var star = Icon(
-      Icons.star,
-      color: Colors.green[500],
-    );
-    var stars = Row(
-      children: [
-        star,
-        star,
-        star,
-        star,
-        star,
-      ],
-    );
-
-    var rating = Text(
-      "170 Reviewss",
-      style: TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.w800,
-        fontFamily: 'Roboto',
-        letterSpacing: 0.5,
-      ),
-    );
-
-    final descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 10,
-      height: 2,
-    );
-
-    var iconList = DefaultTextStyle.merge(
-        style: descTextStyle,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: [Icon(Icons.kitchen), Text("PREP:"), Text("25 min.")],
-            ),
-            Column(
-              children: [Icon(Icons.restaurant), Text("FEEDS:"), Text("4-6.")],
-            ),
-            Column(
-              children: [Icon(Icons.timer), Text("COOK:"), Text("1 hr.")],
-            )
-          ],
-        ));
-
-    var leftColumn = Expanded(
-      child: Container(
-        padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            title,
-            subTitle,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                stars,
-                Expanded(
-                  child: rating,
-                ),
-              ],
-            ),
-            iconList,
-          ],
-        ),
-      ),
-    );
-
-    var rightColumn =
-    Image.asset("lib/assets/1.png", fit:  BoxFit.cover, width: 200, height: double.infinity,)
-    ;
-
-    return Scaffold(
-      body: Card(
-        child: Row(children: [
-          leftColumn,
-          rightColumn,
-        ]),
-      ),
-    );
-  }
+    appBar: AppBar(
+      title: Text("El título"),
+    ),
+    body: _buildImageColumn(),
+  )));
 }
+
+Widget _buildImageColumn() => Container(
+      decoration: BoxDecoration(
+        color: Colors.black26,
+      ),
+      child: Column(
+        children: [
+          _buildImageRow(1),
+          _buildImageRow(3)
+        ],
+      ),
+    );
+
+Widget _buildImageRow(imageIndex) => Row(
+  children: [
+    _buildDecoratedImage(imageIndex),
+    _buildDecoratedImage(imageIndex + 1)
+  ],
+);
+
+Expanded _buildDecoratedImage(int imageIndex) => Expanded(
+  child: Container(
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 40, color: Colors.greenAccent),
+      ),
+      margin: const EdgeInsets.all(4.0),
+      child: Image.asset("images/pic$imageIndex.jpg"),
+    ),
+  )
+);
