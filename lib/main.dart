b89776f39198eceaa009968b57d43afe3845baaa
Anthony Cantuta <anthony@cantuta.org>
2021-04-30 00:28:52 -0500

    usage of ListView

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(body: _buildGrid()),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("The title"),
+      ),
+      body: ListView(
+        children: [
+          _tile("jjijiji 1 ", Icons.ac_unit),
+          _tile("jjijiji 2 ", Icons.star),
+          _tile("jjijiji 3 ", Icons.motorcycle),
+        ],
+      ),
+    ),
   ));
 }
 
-int count = 29;
+ListTile _tile(String title, IconData icon) => ListTile(
+          title: Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          leading: Icon(
+            icon,
+            color: Colors.blue,
+          ),
+        );
 
-Widget _buildGrid() => GridView.count(
-      crossAxisCount: 4,
-      padding: const EdgeInsets.all(4),
-      children: List.generate(count, (index) => Container(child: Image.asset('images/pic${index + 1}.jpg'),)),
-    );
