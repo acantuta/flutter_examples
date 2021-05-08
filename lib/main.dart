9fd606ad8ea088c398ff404ef786c91412b7f263
Anthony Cantuta <anthony@cantuta.org>
2021-05-07 23:53:07 -0500

    Tutorial Layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,36 +8,106 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget imageSection = Image.asset(
+      "images/pic0.jpg",
+      width: double.infinity,
+      height: 400,
+      fit: BoxFit.cover,
+    );
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          //Title
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      "oeschinen lake Campground",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                Text(
+                  "Kandersteg, Switzedland",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red,),
+          Text("41")
+        ],
+      ),
+    );
+
+    Widget buttonSection = Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(Colors.blue, Icons.call, "CALL"),
+        _buildButtonColumn(Colors.blue, Icons.near_me, "ROUTE"),
+        _buildButtonColumn(Colors.blue, Icons.share, "SHARE"),
+      ],
+    ));
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            "A title1111",
+            "Flutter Layout Demo",
           ),
         ),
-        body: Container(
-          width: double.infinity,
-          child: Card(
-            child: Column(children: [
-              buildListTile(Icons.access_alarm, "A title", "A title2"),
-              Divider(),
-              buildListTile(Icons.accessibility_new, "A title", "A title2"),
-              Divider(),
-            ]),
-          ),
+        body: Column(
+          children: [
+            imageSection,
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
     );
   }
 
-  ListTile buildListTile(IconData icon, String title, String subtitle) {
-    return ListTile(
-        leading: Icon(
-          Icons.ac_unit,
-          color: Colors.blue,
-          size: 50,
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: color,
         ),
-        title: Text(title),
-        subtitle: Text(subtitle));
+        Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ))
+      ],
+    );
   }
 }
