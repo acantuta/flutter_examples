37aea2a4db3606b8aae28c614882d7cc211e4c43
Anthony Cantuta <anthony@cantuta.org>
2021-05-11 22:42:51 -0500

    Widget padre gestiona el estado de los widgets hijo.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: TapboxA(),
+        child: ParentWidget(),
       ),
     ));
   }
@@ -47,10 +47,68 @@ class _TapboxAState extends State<TapboxA> {
         height: 200,
         child: Center(
             child: Text(
-              _active ? "Active" : "Inactive",
-            )),
+          _active ? "Active" : "Inactive",
+        )),
         decoration: BoxDecoration(
           color: _active ? Colors.green : Colors.grey,
+        ),
+      ),
+    );
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({Key key}) : super(key: key);
+
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    print("handle tap box changed...");
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        child: Center(
+            child: Text(
+          active ? "Active_" : "Inactive_",
+        )),
+        decoration: BoxDecoration(
+          color: active ? Colors.green : Colors.grey,
         ),
       ),
     );
