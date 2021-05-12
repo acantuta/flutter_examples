62cb6fae13c927b81ef8ad6b8522c54e220251a1
Anthony Cantuta <anthony@cantuta.org>
2021-05-11 22:25:04 -0500

    Gestionando el estado de un Widget
    El widget gestiona su propio estado.

--- lib/main.dart
+++ lib/main.dart
@@ -8,144 +8,51 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget imageSection = Image.asset(
-      "images/pic0.jpg",
-      width: double.infinity,
-      height: 200,
-      fit: BoxFit.cover,
-    );
-
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          //Title
-          Expanded(
-            child: Column(
-              children: [
-                Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(
-                      "oeschinen lake Campground",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )),
-                Text(
-                  "Kandersteg, Switzedland",
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                )
-              ],
-            ),
-          ),
-          FavoriteWidget(),
-        ],
-      ),
-    );
-
-    Widget buttonSection = Container(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(Colors.blue, Icons.call, "CALL"),
-        _buildButtonColumn(Colors.blue, Icons.near_me, "ROUTE"),
-        _buildButtonColumn(Colors.blue, Icons.share, "SHARE"),
-      ],
-    ));
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Flutter Layout Demo",
-          ),
-        ),
-        body: Column(
-          children: [
-            imageSection,
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Flutter Layout Demo",
         ),
       ),
-    );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          color: color,
-        ),
-        Container(
-            margin: const EdgeInsets.only(top: 8),
-            child: Text(
-              label,
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                color: color,
-              ),
-            ))
-      ],
-    );
+      body: Center(
+        child: TapboxA(),
+      ),
+    ));
   }
 }
 
-class FavoriteWidget extends StatefulWidget {
-  const FavoriteWidget({
-    Key key,
-  }) : super(key: key);
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
 
   @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+  _TapboxAState createState() => _TapboxAState();
 }
 
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 40;
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _onTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-        children: [
-      IconButton(
-        icon: _isFavorited ? Icon(Icons.star) : Icon(Icons.star_border),
-        color: Colors.red,
-        onPressed: _toggleFavorite,
+    return GestureDetector(
+      onTap: _onTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        child: Center(
+            child: Text(
+              _active ? "Active" : "Inactive",
+            )),
+        decoration: BoxDecoration(
+          color: _active ? Colors.green : Colors.grey,
+        ),
       ),
-      Text('${_favoriteCount}')
-    ]);
-  }
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount--;
-      } else {
-        _favoriteCount++;
-      }
-      _isFavorited = !_isFavorited;
-    });
+    );
   }
 }
