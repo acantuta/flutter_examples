3bd4ae210cde8cd94ee3d580257470775413202b
Anthony Cantuta <anthony@cantuta.org>
2021-05-19 21:50:03 -0500

    Enviar Datos a una nueva pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  var app = MyApp();
+  runApp(app);
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FirstRoute()
+      home: HomePage(),
     );
   }
 }
 
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({Key key}) : super(key: key);
+class Todo {
+  final String title;
+  final String body;
+
+  Todo(this.title, this.body);
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<Todo> list = List.generate(
+        20, (index) => Todo("The title $index", "The description $index"));
     return Scaffold(
-        appBar: AppBar(
-          title: Text("The title"),
-        ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: () {
-              print("pressed...");
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => SecondRoute(),
-              ));
-            },
-            child: Text("Launch second page33"),
-          ),
-        ));
+      appBar: AppBar(
+        title: Text("Todos"),
+      ),
+      body: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailScreen(todo: list[index])));
+              },
+              title: Center(child: Text(list[index].title)),
+            );
+          }),
+    );
   }
 }
 
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({Key key}) : super(key: key);
+class DetailScreen extends StatelessWidget {
+  final Todo todo;
+
+  const DetailScreen({Key key, @required this.todo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Container(
-        child: Center(
-          child: RaisedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text("Close"),
-          ),
-        ),
+      appBar: AppBar(
+        title: Text(this.todo.title),
       ),
-    ));
+      body: Center(child: Text(this.todo.body)),
+    );
   }
 }
